20c651df28ed190cd56d55274f90d6a13f7783d6
arjun <abemarkar@tesla.com>
2023-09-16 16:32:22 -0700

    add back telescope

--- after/plugin/autocomplete.lua
+++ after/plugin/autocomplete.lua
@@ -14,7 +14,7 @@ cmp.setup({
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ['<TAB>'] = cmp.mapping.confirm({ select = true }),
       ['<C-j>'] = cmp.mapping.select_next_item(),
       ['<C-k>'] = cmp.mapping.select_prev_item(),
     }),
--- after/plugin/fuzzy.lua
+++ after/plugin/fuzzy.lua
@@ -1,5 +1,7 @@
+local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>ff', function() vim.api.nvim_command(':Files') end)
-vim.keymap.set('n', '<leader>fw', function() vim.api.nvim_command(':Rg') end)
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fg', function() vim.api.nvim_command(':GFiles') end)
 vim.keymap.set('n', '<leader>fb', function() vim.api.nvim_command(':BLines') end)
 
