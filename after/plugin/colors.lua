23e09d2c44061dd88affa5e08f6016d78d1fc988
CCLDArjun <ccldarjun@icloud.com>
2023-09-24 13:12:01 -0700

    random colors and fix cscope

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -73,35 +73,55 @@ function catcuppin()
     vim.cmd.colorscheme "catppuccin"
 end
 
--- gruvbox_scheme()
--- catcuppin()
--- vim.cmd.colorscheme "skull"
-local c = require('vscode.colors').get_colors()
--- require('vscode').setup({
---     -- Alternatively set style in setup
---     -- style = 'light'
--- 
---     -- Enable transparent background
---     transparent = true,
--- 
---     -- Enable italic comment
---     italic_comments = true,
--- 
---     -- Disable nvim-tree background color
---     disable_nvimtree_bg = true,
--- 
---     -- Override colors (see ./lua/vscode/colors.lua)
---     color_overrides = {
---         vscLineNumber = '#FFFFFF',
---     },
--- 
---     -- Override highlight groups (see ./lua/vscode/theme.lua)
---     group_overrides = {
---         -- this supports the same val table as vim.api.nvim_set_hl
---         -- use colors from this colorscheme by requiring vscode.colors!
---         Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
---     }
--- })
--- require('vscode').load()
+function vscode()
+    local c = require('vscode.colors').get_colors()
+    require('vscode').setup({
+        -- Alternatively set style in setup
+        -- style = 'light'
+
+        -- Enable transparent background
+        transparent = true,
+
+        -- Enable italic comment
+        italic_comments = true,
+
+        -- Disable nvim-tree background color
+        disable_nvimtree_bg = true,
+
+        -- Override colors (see ./lua/vscode/colors.lua)
+        color_overrides = {
+            vscLineNumber = '#FFFFFF',
+        },
+
+        -- Override highlight groups (see ./lua/vscode/theme.lua)
+        group_overrides = {
+            -- this supports the same val table as vim.api.nvim_set_hl
+            -- use colors from this colorscheme by requiring vscode.colors!
+            Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        }
+    })
+    require('vscode').load()
+    vim.cmd.colorscheme 'vscode'
+end
+
 gruvbox_scheme()
+vscode()
+catcuppin()
+
+colors = {
+    "darkblue",
+    "elflord",
+    "evening",
+    "morning",
+    "peachpuff",
+    "gruvbox",
+    "catppuccin-macchiato",
+    "catppuccin-frappe",
+    "catppuccin-mocha",
+}
+
+math.randomseed(os.date("*t").hour)
+local scheme = colors[math.random(1, #colors)]
+print(scheme)
+vim.cmd("colorscheme " .. scheme)
 
--- lua/arjun/cscope.lua
+++ lua/arjun/cscope.lua
@@ -1,5 +1,6 @@
 require('cscope_maps').setup()
 
+vim.cmd "command Cs Cscope"
 if io.open("cscope.out", "r") then
     vim.cmd("cs add cscope.out")
 end
