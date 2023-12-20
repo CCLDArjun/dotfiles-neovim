5f569e86a917323d4e5c0c28f934d4c12b52bc08
arjun <ccldarjun@icloud.com>
2023-12-20 12:51:54 -0800

    bigger max buffer for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -20,7 +20,7 @@ require'nvim-treesitter.configs'.setup {
 
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
-        local max_filesize = 500 * 1024 -- 500 KB
+        local max_filesize = 2 * 500 * 1024 -- 1000 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
             return true
