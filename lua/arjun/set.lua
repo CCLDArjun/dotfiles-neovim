e51e6ef065a858260f1d08d3abd3b07b298c506c
Arjun Bemarkar <arjun@Arjuns-MacBook-Pro.local>
2024-04-05 23:08:31 -0700

    use exuberant ctags from homebrew

--- lua/arjun/set.lua
+++ lua/arjun/set.lua
@@ -33,6 +33,16 @@ vim.opt.clipboard = unnamedplus
 
 vim.opt.pumheight = 3 -- only want 3 completion items
 
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
+local ctags_path = "/opt/homebrew/bin/ctags"
+if file_exists(ctags_path) then
+    vim.g.tagbar_ctags_bin = ctags_path
+end
+
 require('gitblame').setup {
     enabled = false,
 }
